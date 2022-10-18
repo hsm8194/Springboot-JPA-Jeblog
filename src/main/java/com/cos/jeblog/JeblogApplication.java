1a36b4951dbf3f11da1cc13bb40e1589d51d6391
= <=>
2022-10-18 16:08:04 +0900

    환경세팅완료

---
+++ src/main/java/com/cos/jeblog/JeblogApplication.java
@@ -0,0 +1,13 @@
+package com.cos.jeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeblogApplication.class, args);
+	}
+
+}
